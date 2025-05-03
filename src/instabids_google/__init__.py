338f49aa762180dad46523e20c060c49f208358a
JustinAIDistuptors <justin@automatedaisolutions.ai>
2025-05-03 15:45:20 -0400

    Add vendored Google ADK package

---
+++ src/instabids_google/__init__.py
@@ -0,0 +1,8 @@
+"""
+Vendored copy of Google's Agent Development Kit (ADK).
+
+This package contains a copy of Google's ADK that has been vendored into the
+InstaBids codebase to allow for customization and version control.
+"""
+
+__version__ = "0.2.0"
